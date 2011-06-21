365787c4b803a40c0d2b75670d3686033c0e2854
frank asseg <frank.asseg@fiz-karlsruhe.de>
2011-06-21 12:35:52 +0200

    fixed prefix handling

--- src/test/java/de/fiz/akubra/hdfs/test/HDFSAkubraTCKTest.java
+++ src/test/java/de/fiz/akubra/hdfs/test/HDFSAkubraTCKTest.java
@@ -14,46 +14,36 @@ import de.fiz.akubra.hdfs.HDFSBlobStore;
 
 @Test
 public class HDFSAkubraTCKTest extends TCKTestSuite {
-	private static BlobStore store;
-	private static URI storeId;
-	private static final Logger log = LoggerFactory
-			.getLogger(HDFSAkubraTCKTest.class);
+    private static BlobStore store;
+    private static URI storeId;
+    private static final Logger log = LoggerFactory.getLogger(HDFSAkubraTCKTest.class);
 
-	static {
-		storeId = URI.create("hdfs://localhost:9000/");
-		store = new HDFSBlobStore(storeId);
-	}
+    static {
+        storeId = URI.create("hdfs://localhost:9000/");
+        store = new HDFSBlobStore(storeId);
+    }
 
-	public HDFSAkubraTCKTest() {
-		super(store, storeId, false, true, true, true, true, true, false);
-	}
+    public HDFSAkubraTCKTest() {
+        super(store, storeId, false, true, true, true, true, true, false);
+    }
 
-	@Override
-	protected URI[] getAliases(URI arg0) {
-		return new URI[] { arg0 };
-	}
+    @Override
+    protected URI[] getAliases(URI arg0) {
+        return new URI[] { arg0 };
+    }
 
-	@Override
-	protected URI getInvalidId() {
-		return URI.create("urifoo:zt");
-	}
+    @Override
+    protected URI getInvalidId() {
+        return URI.create("urifoo:zt");
+    }
 
-	@Override
-	protected URI createId(String name) {
-		return URI.create(store.getId() + name);
-	}
+    @Override
+    protected URI createId(String name) {
+        return URI.create(store.getId() + name);
+    }
 
-	@Override
-	protected String getPrefixFor(String name) {
-		return name;
-	}
-	@AfterClass
-	public void cleanup() throws Exception{
-		BlobStoreConnection conn=store.openConnection(null, null);
-		conn.getBlob(URI.create(storeId.toASCIIString() + "blobBasicList1"), null).delete();
-		conn.getBlob(URI.create(storeId.toASCIIString() + "blobMoveTo1"), null).delete();
-		conn.getBlob(URI.create(storeId.toASCIIString() + "blobMoveTo2"), null).delete();
-		conn.getBlob(URI.create(storeId.toASCIIString() + "blobMoveTo4"), null).delete();
-	}
-
+    @Override
+    protected String getPrefixFor(String name) {
+        return name;
+    }
 }
