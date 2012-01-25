b7ebf3885e2e83b3d7ec03fd8ac4ffd51033a00a
frank asseg <frank.asseg@fiz-karlsruhe.de>
2012-01-25 11:23:36 +0100

    Added MiniDFSCluster for easy test execution

--- src/test/java/de/fiz/akubra/hdfs/test/HDFSAkubraTCKTest.java
+++ src/test/java/de/fiz/akubra/hdfs/test/HDFSAkubraTCKTest.java
@@ -1,45 +1,72 @@
 package de.fiz.akubra.hdfs.test;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 
-import org.akubraproject.BlobStore;
 import org.akubraproject.tck.TCKTestSuite;
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.apache.hadoop.hdfs.server.common.HdfsConstants.StartupOption;
+import org.apache.hadoop.hdfs.server.namenode.NameNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import de.fiz.akubra.hdfs.HDFSBlobStore;
 
 public class HDFSAkubraTCKTest extends TCKTestSuite {
-    private static BlobStore store;
-    private static URI storeId;
-    private static final Logger log = LoggerFactory.getLogger(HDFSAkubraTCKTest.class);
+	private static final Logger log = LoggerFactory.getLogger(HDFSAkubraTCKTest.class);
+	private static MiniDFSCluster cluster;
 
-    static {
-        storeId = URI.create("hdfs://localhost:9000/");
-        store = new HDFSBlobStore(storeId);
-    }
+	@BeforeClass
+	public static void startCluster() throws Exception {
+			File nameDir=new File("target/test-namenode");
+			File dataDir=new File("target/test-datanode");
+			FileUtils.deleteDirectory(nameDir);
+			FileUtils.deleteDirectory(dataDir);
+			nameDir.mkdir();
+			dataDir.mkdir();	
+			Configuration cfg = new Configuration();
+			cfg.set("dfs.name.dir", nameDir.getAbsolutePath());
+			cfg.set("dfs.data.dir", dataDir.getAbsolutePath());
+			cfg.set("fs.default.name","hdfs://localhost:9000/");
+			NameNode.format(cfg);
+			cluster = new MiniDFSCluster(9000,cfg,1,false,false,StartupOption.FORMAT,null);
+			cluster.waitActive();
+		
+	}
 
-    public HDFSAkubraTCKTest() {
-        super(store, storeId, false, true, true, true, true, true, false);
-    }
+	@AfterClass
+	public static void stopCluster() throws Exception {
+		cluster.shutdownDataNodes();
+		cluster.shutdownNameNode();
+		cluster.shutdown();
+	}
 
-    @Override
-    protected URI[] getAliases(URI arg0) {
-        return new URI[] { arg0 };
-    }
+	public HDFSAkubraTCKTest() {
+		super(new HDFSBlobStore(URI.create("hdfs://localhost:9000/")), URI.create("hdfs://localhost:9000/"), false, true, true, true, true, true, false);
+	}
 
-    @Override
-    protected URI getInvalidId() {
-        return URI.create("urifoo:zt");
-    }
+	@Override
+	protected URI[] getAliases(URI arg0) {
+		return new URI[] { arg0 };
+	}
 
-    @Override
-    protected URI createId(String name) {
-        return URI.create(store.getId() + name);
-    }
+	@Override
+	protected URI getInvalidId() {
+		return URI.create("urifoo:zt");
+	}
 
-    @Override
-    protected String getPrefixFor(String name) {
-        return name;
-    }
+	@Override
+	protected URI createId(String name) {
+		return URI.create(store.getId() + name);
+	}
+
+	@Override
+	protected String getPrefixFor(String name) {
+		return name;
+	}
 }
